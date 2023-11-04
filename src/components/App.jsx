9710d597e3ab21c387e8ac8ee4bdda80eaf9b929
Eduard <sorokolietov@gmail.com>
2023-11-04 22:42:50 +0100

    commit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Section } from './Section/Section';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
